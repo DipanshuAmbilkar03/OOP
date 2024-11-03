3af8138cfb204313298e7bbd5ead1274dfa30490
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2024-11-03 11:31:04 +0530

    oop lab experiment 1

--- experiment1.cpp
+++ experiment1.cpp
@@ -1,7 +1,5 @@
-/*
-
-Implement a class Complex which represents the Complex Number data type. Implement 
-thefollowing
+/*Implement a class Complex which represents the Complex Number data type. Implement 
+thefollowing :
         1. Constructor (including a default constructor which creates the complex number 0+0i).
         2. Overload operator+ to add two complex numbers.
         3. Overload operator* to multiply two complex numbers.
