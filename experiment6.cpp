21bb8fdf71c720586603f5dd843cb56e2a56cfe4
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2024-11-03 12:11:04 +0530

    oop experiment 6

--- experiment6.cpp
+++ experiment6.cpp
@@ -1,6 +1,3 @@
-/*Write C++ program using STL for sorting and searching user defined records such as 
-personal records (Name, DOB, Telephone number etc) using vector container. */
-
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -13,13 +10,7 @@ struct personal_record {
     string telephone_number;
 
     personal_record(string name, string dob, string telephone_number)
-        : name(name),dob(dob),telephone_number(telephone_number) {}
-    
-    // {
-    //     this->name = name;
-    //     this->dob = dob;
-    //     this->telephone_number = telephone_number;
-    // }
+        : name(name), dob(dob), telephone_number(telephone_number) {}
 
     bool operator<(const personal_record &other) const {
         return name < other.name;
@@ -32,15 +23,16 @@ struct personal_record {
 
 void display(const vector<personal_record> &records) {
     for (const auto &record : records) {
-        cout << "Name: " << record.name <<","<<" DOB: " << record.dob<<","
-            << " Telephone: " << record.telephone_number << endl;
+        cout << "Name: " << record.name << ", DOB: " << record.dob << ", Telephone: " << record.telephone_number << endl;
     }
 }
 
 int main() {
     vector<personal_record> records;
-    records.push_back(personal_record("John Doe", "1990-05-15", "1234567890"));
-    records.push_back(personal_record("Jane Smith", "1985-12-25", "0987654321"));
+    records.push_back(personal_record("onyx", "1987-03-15", "9876543210"));
+    records.push_back(personal_record("riya", "1992-07-22", "9123456789"));
+    records.push_back(personal_record("zich", "1990-11-30", "9988776655"));
+    records.push_back(personal_record("binod", "1985-12-25", "9765432101"));
 
     cout << "Records before sorting:" << endl;
     display(records);
@@ -50,17 +42,17 @@ int main() {
     cout << "Records after sorting:" << endl;
     display(records);
 
-    string search_name = "John Doe";
-    personal_record search_record(search_name, "","");
+    string search_name = "mahi";
+    personal_record search_record(search_name, "", "");
     bool found = binary_search(records.begin(), records.end(), search_record);
 
     if (!found) {
-        cout << "Name not found in records." << endl;
+            cout << "Name not found in records." <<endl;
     } else {
         cout << "Name found in records." << endl;
     }
 
-    search_name = "Michael";
+    search_name = "rahul";
     search_record = personal_record(search_name, "", "");
     found = binary_search(records.begin(), records.end(), search_record);
 
@@ -69,6 +61,8 @@ int main() {
     } else {
         cout << "Name found in records." << endl;
     }
-
     return 0;
+
+
 }
+
